8d6f50c2447723f897e914eb5847b56352ef5c28
Na-gang99 <naagy0814@gmail.com>
2024-06-02 23:18:27 +0900

    change ServiceRegistrationList

--- plog/src/pages/ServiceRegistration/ServiceRegistrationList.js
+++ plog/src/pages/ServiceRegistration/ServiceRegistrationList.js
@@ -2,19 +2,30 @@ import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../../styles/Table.css';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import Pagination from 'react-js-pagination';
 
 function ServiceRegistrationList() {
   const navigate = useNavigate();
   const [registrations, setRegistrations] = useState([]);
   const userId = localStorage.getItem('userId');
   const accessToken = localStorage.getItem('accesToken');
-
+  const [itemsPerPage, setItemsPerPage] = useState(window.innerWidth < 1000 ? 1 : 5);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortKey, setSortKey] = useState('providerName');
+  
   useEffect(() => {
     if (!accessToken) {
-        navigate("/signin");
+      navigate("/signin");
+    } else {
+      getServiceRegistrationList(userId);
     }
-    else{
-    getServiceRegistrationList(userId);}
+    const handleResize = () => {
+      setItemsPerPage(window.innerWidth < 1000 ? 1 : 5);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, [accessToken, userId, navigate]);
 
   const handleServiceRegistrationClick = () => {
@@ -23,85 +34,129 @@ function ServiceRegistrationList() {
 
   const getServiceRegistrationList = async () => {
     try {
-      const accessToken = localStorage.getItem('accesToken'); 
-
       const response = await axios.get(`${process.env.REACT_APP_URL}/service/check`, {
         params: {
           userId: userId,
         },
         headers: {
-          'Auth-Token': accessToken, 
+          'Auth-Token': accessToken,
         },
       });
 
       if (response.status === 200) {
         setRegistrations(response.data);
-        console.log(response.data);
-    }
-  } catch (error) {
+      }
+    } catch (error) {
       console.error('서비스등록내역을 불러오는 중에 문제가 발생했습니다.', error);
       if (error.response && error.response.status === 400) {
         alert('잘못된 요청 형식입니다. 다시 시도해 주세요.');
-      } 
-      else if (error.response && error.response.status === 401) {
+      } else if (error.response && error.response.status === 401) {
         alert("로그인 만료. 다시 로그인해주세요.");
         navigate('/signin', { replace: true });
-      } 
-      else {
+      } else {
         alert('서비스등록내역을 불러오는 중에 문제가 발생했습니다.');
       }
     }
   };
 
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const sortRegistrations = (key) => {
+    setSortKey(key);
+  };
+
+  useEffect(() => {
+    if (sortKey) {
+      setRegistrations((prevList) => {
+        return [...prevList].sort((a, b) => {
+          if (a[sortKey] < b[sortKey]) {
+            return -1;
+          }
+          if (a[sortKey] > b[sortKey]) {
+            return 1;
+          }
+          return 0;
+        });
+      });
+    }
+  }, [sortKey]);
+
+  const indexOfLastRegistration = currentPage * itemsPerPage;
+  const indexOfFirstRegistration = indexOfLastRegistration - itemsPerPage;
+  const currentRegistrations = registrations.slice(indexOfFirstRegistration, indexOfLastRegistration);
+
   return (
     <div className='Table'>
-      <h4>서비스등록하기</h4>
-      <button 
-      onClick={handleServiceRegistrationClick}
-      style={{
-        width: '15%',
-        padding: '5px',
-        marginBottom: '10px',
-        boxSizing: 'border-box',
-        borderRadius: '15px',
-        backgroundColor: '#162617',
-        color: '#E8EEE8',
-        fontWeight: 'bold'
-      }}
-      >서비스등록</button>
+      <h4>서비스등록</h4>
+      <div style={{
+        display: "flex",
+        justifyContent: "center", 
+        alignItems: "center",
+      }}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div className="sort-buttons">
+          <button className="sort" onClick={() => sortRegistrations('providerName')}>이름순</button>
+          <button className="sort" onClick={() => sortRegistrations('providerType')}>종류순</button>
+          <button className="sort" onClick={() => sortRegistrations('providerStatus')}>상태순</button>
+        </div>
+      </div>
+      <button
+        onClick={handleServiceRegistrationClick}
+        style={{
+          marginBottom: '10px',
+          marginLeft: '15px',
+        }}
+      >
+        서비스등록
+      </button>
+      </div>
       {registrations.length === 0 ? (
         <p>서비스 등록을 신청한 이력이 없습니다.</p>
       ) : (
-      <table>
-  <thead>
-    <tr>
-      <th>서비스 이름</th>
-      <th>서비스 종류</th>
-      <th>상태</th>
-    </tr>
-  </thead>
-  <tbody>
-    {registrations.map((registration, index) => (
-      <tr key={index}>
-        <td>{registration.providerName}</td>
-        <td>
-          {registration.providerType === 1 ? "사진작가" : 
-           registration.providerType === 2 ?  "헤어,메이크업": 
-           "스튜디오"}
-        </td>
-        <td>
-          {registration.providerStatus === 1 ? "승인" :
-          registration.providerStatus === 2 ? "대기중" : 
-          registration.providerStatus === 3 ? "정지" : "반려"
-          }
-      </td>
-      </tr>
-    ))}
-  </tbody>
-  </table>)}
-  </div>
+        <>
+          <Table style={{ width: window.innerWidth <= 1000 ? '60%' : '40%'}}>
+            <Thead>
+              <Tr>
+                <Th>서비스 이름</Th>
+                <Th>서비스 종류</Th>
+                <Th>상태</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {currentRegistrations.map((registration, index) => (
+                <Tr key={index}>
+                  <Td><div className='text'>{registration.providerName}</div></Td>
+                  <Td>
+                    <div className='text'>{registration.providerType === 1 ? "사진작가" :
+                      registration.providerType === 2 ? "헤어,메이크업" :
+                        "스튜디오"}
+                    </div>
+                  </Td>
+                  <Td>
+                    <div className='text'>
+                      {registration.providerStatus === 1 ? "승인" :
+                        registration.providerStatus === 2 ? "대기중" :
+                          registration.providerStatus === 3 ? "정지" : "반려"
+                      }
+                    </div>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+          <Pagination
+            activePage={currentPage}
+            itemsCountPerPage={itemsPerPage}
+            totalItemsCount={registrations.length}
+            pageRangeDisplayed={5}
+            onChange={handlePageChange}
+          />
+        </>
+      )}
+    </div>
   );
 }
 
 export default ServiceRegistrationList;
-
